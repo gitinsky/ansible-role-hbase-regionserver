0334350befd568e5df04eb1634138616beb232cd
Alex Chistyakov <alexclear@gmail.com>
2015-07-10 10:12:47 +0300

    Make path and user configurable

--- templates/hbase/regionserver.rb
+++ templates/hbase/regionserver.rb
@@ -1,12 +1,12 @@
-Eye.application 'hbase-regionserver' do
+Eye.application 'hbase-regionserver-{{ env_name }}' do
   working_dir '/etc/eye'
-  stdall '/var/log/eye/hbase-regionserver-stdall.log' # stdout,err logs for processes by default
+  stdall '/var/log/eye/hbase-regionserver-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
 
-  process :regionserver do
-    pid_file '/var/hbase/hbase_regionserver.pid'
-    start_command 'sudo -u hadoop /opt/hbase/bin/hbase regionserver start'
+  process :regionserver_{{ env_name }} do
+    pid_file '{{ hbase_var_prefix }}/hbase_regionserver.pid'
+    start_command 'sudo -u {{ hadoop_user }} {{ hbase_distr_prefix }}/bin/hbase regionserver start'
 
     daemonize true
     start_timeout 10.seconds
